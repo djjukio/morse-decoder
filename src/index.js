f65bbd9f8db9034eba4d89c733011d350f66d87a
Andrey Alfirovich <djjukio@gmail.com>
2020-03-03 20:46:03 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let length = expr.length / 10;   
+    let letters = [];
+    for (let key in MORSE_TABLE) {  
+        let changer;
+        changer = key;
+        delete key;
+        key = MORSE_TABLE[key];
+        MORSE_TABLE[key] = changer;
+    };
+   
+    for (let key in MORSE_TABLE) {    
+        let arrCode = MORSE_TABLE[key].split("");
+        for (x = 0; x < arrCode.length; x++) {
+            if (arrCode[x] == ".") {
+                arrCode[x] = "10";
+            } else if (arrCode[x] == "-") {
+                arrCode[x] = "11";
+            };
+        };
+        MORSE_TABLE[key] = arrCode.join("");
+    };
+
+        
+    for(let i = 0; i < length; i++) { 
+        letters[i] = expr.substr(i*10, 10);
+    };
+    let cleanLetters = letters.map(function(letter) { 
+        for(let j = 0; j < 10; j++) {
+            if ( letter[j] == "1") {
+               return letter = letter.slice(j);
+            } else if (letter[j] == "*") {
+                return letter = " ";
+            };
+        };
+    });
+    let words = cleanLetters.map(function(item) {
+        for (let key in MORSE_TABLE) {
+            if ( MORSE_TABLE[key] == item) {
+                return item = key;
+            } else if ( item == " ") {
+            	return item = " ";
+            };
+        };
+    }); 
+    return words.join(""); 
+};
 
 module.exports = {
     decode
-}
+} 
